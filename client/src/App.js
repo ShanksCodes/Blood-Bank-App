8a761843f33b107f3478b31807678ecf9687a79b
ShanksCodes <abhishektyagiofficial99@gmail.com>
2023-10-15 22:11:04 +0530

    doing some frontend

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,39 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage.js";
 import Register from "./pages/auth/Register.js";
 import Login from "./pages/auth/Login.js";
+import { ToastContainer } from "react-toastify";
+import ProtectedRoute from "./components/Routes/ProtectedRoute";
+import PublicRoute from "./components/Routes/PublicRoute";
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoute>
+              <Register />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
       </Routes>
     </>
   );
